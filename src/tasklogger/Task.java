6fbdff0475fa03ea7d0e29595df55fc205a05f47
Nathan <nathan@mattick.name>
2016-06-24 22:11:02 +0100

    Friday work update

--- src/tasklogger/Task.java
+++ src/tasklogger/Task.java
@@ -1,9 +1,10 @@
 package tasklogger;
 
-// Nathan
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
@@ -11,100 +12,113 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Task {
-  private TimerTask timerTask;
-  private Timer timer;
-  private int seconds;
-  private Boolean running;
-  private String name;
-  private ActionListener actionListender;
-  private int taskID;
+	private TimerTask timerTask;
+	private Timer timer;
+	private int seconds;
+	private Boolean running;
+	private String name;
+	private ActionListener actionListender;
+	private int taskID;
+	private PropertyChangeSupport pcs;
 
-  public Task() {
-    taskID = System.identityHashCode(this);
-    seconds = 0;    
-    running = new Boolean(false);
-    
-    actionListender = new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        System.out.println("AL=" + getActionListener());
-        String command = e.getActionCommand();
-        if (command.equals("taskButtonPressed")) {
-          actionTask();
-        }
-      }
-    };
-  }
+	public Task() {
+		taskID = System.identityHashCode(this);
+		seconds = 0;		
+		running = new Boolean(false);
 
-  protected void actionTask() {
-    if (!running) {
-      try {
-        start();
-      } catch (Exception e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-    }
-    else {
-      cancel();
-    }
-  }
+		pcs = new PropertyChangeSupport(this);
 
-  private void start() throws Exception {
-    timer = new Timer();
-    timerTask = new TimerTask() {           
-      @Override
-      public void run() {
-        // Update text with hh:mm:ss count
-        System.out.println("hms=" + convertSecondToHHMMString(seconds));
-        TaskLogger.taskPulse(Task.this);
-        seconds++;
-      }
-    };
 
-    timer.scheduleAtFixedRate(timerTask, 0, 1000);
-    running = true;
-  }
+		actionListender = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("AL=" + getActionListener());
+				String command = e.getActionCommand();
+				if (command.equals("taskButtonPressed")) {
+					actionTask();
+				}
+			}
+		};
+	}
 
-  protected void cancel() {
-    if (timer != null) { 
-      timer.cancel();
-      running = false;
-    }
-  }
+	public void addPropertyChangeListener(PropertyChangeListener l) {
+		pcs.addPropertyChangeListener(l);
+	}
 
-  private String convertSecondToHHMMString(int seconds)
-  {
-    TimeZone tz = TimeZone.getTimeZone("UTC");
-    SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
-    df.setTimeZone(tz);
-    String time = df.format(new Date(seconds*1000L));
-    return time;
-  }
+	public void removePropertyChangeListener(PropertyChangeListener l) {
+		pcs.removePropertyChangeListener(l);
+	}
 
-  public String getHMSString() {
-    return(convertSecondToHHMMString(seconds));
-  }
+	protected void actionTask() {
+		if (!running) {
+			try {
+				start();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else {
+			cancel();
+		}
+	}
 
-  public Boolean getTaskState() {
-    return(running);
-  }
+	private void start() throws Exception {
+		pcs.firePropertyChange("task", running.booleanValue(), !running.booleanValue());
+		timer = new Timer();
+		timerTask = new TimerTask() {						
+			@Override
+			public void run() {
+				// Update text with hh:mm:ss count
+				System.out.println("hms=" + convertSecondToHHMMString(seconds));
+				TaskLogger.taskPulse(Task.this);
+				seconds++;
+			}
+		};
 
-  public void setTitle(String taskName) {
-    // TODO Auto-generated method stub
-    name = taskName;
-  }
+		timer.scheduleAtFixedRate(timerTask, 0, 1000);
+		running = true;
+	}
 
-  public String getName() {   
-    return name;
-  }
+	protected void cancel() {
+		if (timer != null) { 
+			timer.cancel();
+			running = false;
+		}
+	}
 
-  public ActionListener getActionListener() {
-    return(actionListender);
-  }
+	private String convertSecondToHHMMString(int seconds)
+	{
+		TimeZone tz = TimeZone.getTimeZone("UTC");
+		SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+		df.setTimeZone(tz);
+		String time = df.format(new Date(seconds*1000L));
+		return time;
+	}
 
-  public int getTaskID() {
-    return taskID;
-  }
+	public String getHMSString() {
+		return(convertSecondToHHMMString(seconds));
+	}
+
+	public Boolean getTaskState() {
+		return(running);
+	}
+
+	public void setTitle(String taskName) {
+		// TODO Auto-generated method stub
+		name = taskName;
+	}
+
+	public String getName() {		
+		return name;
+	}
+
+	public ActionListener getActionListener() {
+		return(actionListender);
+	}
+
+	public int getTaskID() {
+		return taskID;
+	}
 }
 
--- src/tasklogger/TaskButton.java
+++ src/tasklogger/TaskButton.java
@@ -1,27 +1,43 @@
 package tasklogger;
 
 import java.awt.Color;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import javax.swing.JButton;
 
 public class TaskButton extends JButton {
 
-  private static final long serialVersionUID = -9193221835511157635L;
+	private static final long serialVersionUID = -9193221835511157635L;
+	private int taskId;
+	private Task task;
 
-  public TaskButton(final Task inTask) {
-    setBackground(Color.green);
-    setActionCommand("taskButton");
-    stop();
-  }
+	public TaskButton(final Task inTask) {
+		task = inTask;
+		setBackground(Color.green);
+		setActionCommand("taskButton");
+		stop();
+	}
 
-  public void start() {
-    setText("Stop Task");
-    setBackground(Color.red);
-    repaint();
-  }
+	public void start() {
+		setText("Stop Task");
+		setBackground(Color.red);
+		repaint();
+	}
 
-  public void stop() {
-    setText("Start Task");
-    setBackground(Color.green);
-    repaint();
-  }
+	public void stop() {
+		setText("Start Task");
+		setBackground(Color.green);
+		repaint();
+	}
+
+	public static class MyPropertyChangeListener implements PropertyChangeListener {
+		// This method is called every time the property value is changed
+		public void propertyChange(PropertyChangeEvent evt) {
+			System.out.println("Name = " + evt.getPropertyName());
+			System.out.println("Old Value = " + evt.getOldValue());
+			System.out.println("New Value = " + evt.getNewValue());
+			System.out.println("**********************************");
+		}
+	}
 }
