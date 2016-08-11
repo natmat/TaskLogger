d6463b6ee3af34d2d84028075a070ba3ce43278c
Nathan <nathan@mattick.name>
2016-08-11 22:08:35 +0100

    Update from work

--- src/tasklogger/ExcelReader.java
+++ src/tasklogger/ExcelReader.java
@@ -1,11 +1,14 @@
 package tasklogger;
 
+import org.apache.poi.ss.formula.functions.Column;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 //import org.apache.poi.xssf.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.sun.org.apache.xpath.internal.axes.IteratorPool;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,6 +20,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -26,8 +30,16 @@ import javax.swing.SwingUtilities;
  */
 public class ExcelReader {
 
-//	private static final String FILE_PATH = "/Users/Nathan/github/TaskLogger/src/file.xlsx";
-	private static final String FILE_PATH = "C:/My_Workspaces/MyJava/TaskLogger/src/tasklogger/file.xlsx";
+	// private static final String FILE_PATH =
+	// "/Users/Nathan/github/TaskLogger/src/file.xlsx";
+	// private static final String FILE_PATH =
+	// "C:/My_Workspaces/MyJava/TaskLogger/src/tasklogger/file.xlsx";
+	// private static final String FILE_PATH =
+	// "//Greenlnk.net/Data/Olympia/MA&I/Team Site Folders/B2004MLA/Private/CAGE/Tracking/Cost Tracking Typhoon/008 Jan 2016/Typhoon Cost Tracker Jan 2016 v2.xlsm";
+//	private static final String FILE_PATH = "C:/My_Workspaces/MyJava/TaskLogger/src/tasklogger/typhoon.xlsm";
+	private static final String FILE_PATH = "resources/typhoon.xlsm";
+
+	// private static final String FILE_PATH = "typhoon.xlsm";
 
 	public static void main(String args[]) {
 		SwingUtilities.invokeLater(new Runnable() {
@@ -36,12 +48,9 @@ public class ExcelReader {
 			}
 		});
 	}
-	
+
 	public static void createAndShowGUI() {
 		List<Task> taskList = getTaskNamesFromExcel();
-		for (Task t : taskList) {
-			System.out.println(t.getCode() + ":" + t.getInfo());
-		}
 
 		ArrayList<String> wbc = new ArrayList<>();
 		for (Task t : taskList) {
@@ -49,16 +58,17 @@ public class ExcelReader {
 		}
 		String[] arr = wbc.toArray(new String[wbc.size()]);
 		JComboBox<String> jcb = new JComboBox<>(arr);
+		jcb.setMaximumRowCount(40);
 
 		JFrame frame = new JFrame("JComboBox");
-//		frame.setSize(500, 500);
+		// frame.setSize(500, 500);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		JPanel panel = new JPanel();
 		panel.add(jcb);
 		frame.add(panel);
 		frame.pack();
-		
+
 		jcb.setEditable(true);
 		jcb.setVisible(true);
 		jcb.setSelectedIndex(0);
@@ -66,8 +76,8 @@ public class ExcelReader {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JComboBox<String> cb = (JComboBox<String>)e.getSource();
-				String s = (String)cb.getSelectedItem();
+				JComboBox<String> cb = (JComboBox<String>) e.getSource();
+				String s = (String) cb.getSelectedItem();
 				System.out.println("selected=" + s);
 			}
 		});
@@ -77,73 +87,83 @@ public class ExcelReader {
 		List<Task> taskList = new ArrayList<Task>();
 		FileInputStream fis = null;
 		try {
+			JFileChooser fileChooser = new JFileChooser();
+			
+			
 			fis = new FileInputStream(FILE_PATH);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return(null);
+		}
 
-			// Using XSSF for xlsx format, for xls use HSSF
-			Workbook workbook = new XSSFWorkbook(fis);
+		// Using XSSF for xlsx format, for xls use HSSF
+		Workbook workbook;
+		try {
+			workbook = new XSSFWorkbook(fis);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return(null);
+		}
 
-			int numberOfSheets = workbook.getNumberOfSheets();
+		int numberOfSheets = workbook.getNumberOfSheets();
 
-			// looping over each workbook sheet
-			for (int i = 0; i < numberOfSheets; i++) {
-				Sheet sheet = workbook.getSheetAt(i);
-				Iterator<Row> rowIterator = sheet.iterator();
+		// looping over each workbook sheet
+		for (int i = 0; i < numberOfSheets; i++) {			
+			Sheet sheet = workbook.getSheetAt(i);
+			if (!sheet.getSheetName().equals("WBS")) {
+				continue;				
+			}
+			
+			// iterating over each row
+			Iterator<Row> rowIterator = sheet.iterator();
+			while (rowIterator.hasNext()) {
 
-				// iterating over each row
-				while (rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
 
-					Task task = new Task();
-					Row row = rowIterator.next();
-					Iterator<Cell> cellIterator = row.cellIterator();
+				// Iterating over each cell (column wise) in a particular row.
+				Task task = null;
+				while (cellIterator.hasNext()) {
 
-					// Iterating over each cell (column wise) in a particular
-					// row.
-					while (cellIterator.hasNext()) {
-
-						Cell cell = cellIterator.next();
-						// The Cell Containing String will is name.
-						switch (cell.getColumnIndex()) {
-						case 0:
-							if (Cell.CELL_TYPE_STRING == cell.getCellType()) {
-								task.setCode(cell.getStringCellValue());
-							}
-							break;
-						case 1:
-						default:
-							if (Cell.CELL_TYPE_STRING == cell.getCellType()) {
-								task.setInfo(cell.getStringCellValue());
-							}
-							break;
+					Cell cell = cellIterator.next();
+					// The Cell Containing String will is name.
+					switch (cell.getColumnIndex()) {
+					case 1:
+						if ((Cell.CELL_TYPE_STRING == cell.getCellType()) 
+								&& (cell.getStringCellValue().length() != 0)
+								&& (cell.getStringCellValue().startsWith("D"))) {
+							task = new Task();
+							task.setCode(cell.getStringCellValue());
+							taskList.add(task);
 						}
+						break;
+						
+					default:
+						break;
 					}
-					// end iterating a row, add all the elements of a row in
-					// list
-					taskList.add(task);
 				}
 			}
+		}
 
+		try {
 			fis.close();
 			workbook.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 		return taskList;
 	}
 
 	private static class Task {
 		private String code;
 		private String info;
-		private static int size;
-		
+
 		public Task() {
-			size = 0;
-		}
-		
-		public static int getSize() {
-			return(size);	
+			// TODO
 		}
 
 		public void setCode(String stringCellValue) {
